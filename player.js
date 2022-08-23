4522bdb349193446486372bce422e0cbaf8da1be
Shahadath049 <zamirshahadath049@gamail.com>
2022-08-23 22:12:35 +0600

    final commit

--- player.js
+++ player.js
@@ -1,55 +1,41 @@
 
+//add selected player
 
-document.getElementById('calculate-btn').addEventListener('click',function(){
-
-  const playerPrice =   document.getElementById('player-price');
-  const playerPriceString = playerPrice.value;
-  const playerPriceValue = parseInt(playerPriceString);
-  
-  if(isNaN (playerPriceValue)){
-    alert("Please Enter a Valid Data")
-    return;
-  }
-
-    let parents = document.querySelectorAll(".item");
-    let parentsLength = parents.length;
-
-    const perPlayer = (parentsLength)* playerPriceValue;
-
-    const playerCost = document.getElementById('total-player-cost'); 
-    playerCost.innerText = perPlayer;
-    playerPrice.value = '';
-
-
-})
-document.getElementById('total-btn').addEventListener('click',function(){
-    const manager = document.getElementById('manager-price');
-    const managerStrng = manager.value;
-    const managerprice = parseInt(managerStrng);
-    manager.value = '';
-    if(isNaN (managerprice)){
-        alert("Please Enter a Valid Data")
-        return;
-      }
-
-    const coach = document.getElementById('coach-price');
-    const coachString = coach.value;
-    const coachPrice = parseInt(coachString);
-    coach.value = '';
-    if(isNaN (coachPrice)){
-        alert("Please Enter a Valid Data")
-        return;
-      }
-
-    const playerCost = document.getElementById('total-player-cost');
-    const playerCostString = playerCost.innerText;
-    const playerCostValue = parseInt(playerCostString); 
-
-    const total = managerprice + coachPrice+ playerCostValue;
-    console.log(total);
-
-    const finalExpense = document.getElementById('team-expense');
-    finalExpense.innerText = total
+document.getElementById('messi-btn').addEventListener('click',function(){
+    const all = selectPlayerById("messi-name","messi-btn");
+    
+    
+    
 
     
 })
+document.getElementById('sanches-btn').addEventListener('click',function(){
+    selectPlayerById("sanches-name","sanches-btn");
+    
+})
+
+document.getElementById('machado-btn').addEventListener('click',function(){
+    selectPlayerById("machado-name","machado-btn");
+    
+    
+})
+document.getElementById('mbappe-btn').addEventListener('click',function(){
+    selectPlayerById("mbappe-name","mbappe-btn");
+    
+    
+})
+document.getElementById('neymar-btn').addEventListener('click',function(){
+    selectPlayerById("neymar-name","neymar-btn");
+    
+    
+})
+document.getElementById('ramos-btn').addEventListener('click',function(){
+    selectPlayerById("ramos-name","ramos-btn");
+    
+   
+
+})
+
+
+ 
+
