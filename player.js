0e9793633f6d209adc902e35b6acf4e62cbffa3f
Shahadath049 <zamirshahadath049@gamail.com>
2022-08-23 19:48:31 +0600

    budget calculation fixed

--- player.js
+++ player.js
@@ -5,14 +5,20 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
   const playerPrice =   document.getElementById('player-price');
   const playerPriceString = playerPrice.value;
   const playerPriceValue = parseInt(playerPriceString);
+  
+  if(isNaN (playerPriceValue)){
+    alert("Please Enter a Valid Data")
+    return;
+  }
 
-    // let parents = document.querySelectorAll(".item");
-    // let parentsLength = parents.length;
+    let parents = document.querySelectorAll(".item");
+    let parentsLength = parents.length;
 
-    const perPlayer = 5* playerPriceValue;
+    const perPlayer = (parentsLength)* playerPriceValue;
 
     const playerCost = document.getElementById('total-player-cost'); 
     playerCost.innerText = perPlayer;
+    playerPrice.value = '';
 
 
 })
@@ -20,11 +26,20 @@ document.getElementById('total-btn').addEventListener('click',function(){
     const manager = document.getElementById('manager-price');
     const managerStrng = manager.value;
     const managerprice = parseInt(managerStrng);
-
+    manager.value = '';
+    if(isNaN (managerprice)){
+        alert("Please Enter a Valid Data")
+        return;
+      }
 
     const coach = document.getElementById('coach-price');
     const coachString = coach.value;
     const coachPrice = parseInt(coachString);
+    coach.value = '';
+    if(isNaN (coachPrice)){
+        alert("Please Enter a Valid Data")
+        return;
+      }
 
     const playerCost = document.getElementById('total-player-cost');
     const playerCostString = playerCost.innerText;
